843e9e3252720a4ebadda2c5c7a523370253197b
arnaud <arnaud3488@gmail.com>
2025-03-26 15:38:37 +0700

    update intro text

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -6,10 +6,9 @@ export default function About() {
   return (
     <div className="space-y-12 pb-28">
       <div className="space-y-3">
-        <p>Hello, I am Arnaud,</p>
-        <p>Born and raised into React ecosystem, I love crafting interfaces and building polished web experiences, pixels by pixels.</p>
+        <p>Born and raised into Javascript ecosystem, I love crafting interfaces and building amazing web experiences.</p>
         <p>I develop my skills through doing and experimenting, always searching for those small magical details that make a big impact.</p>
-        <p>Mindful that everything around me is someone’s life work.</p>
+        <p>Mindful that our world is moving fast, I always try to stay up-to-date with the latest trends and technologies.</p>
         <p>If you’re looking to build something beautiful together or just want to chat about tech and web, feel free to reach out!</p>
       </div>
       <TechStack />
